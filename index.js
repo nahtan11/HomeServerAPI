ce1dce61b55bab8a90133ac7313cffdb5bc7712f
Nathan <nathanbwhelan98@gmail.com>
2023-05-07 20:02:57 +0100

    Added new request and db connection

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require('express')
 const app = express()
-
 const port  = 3000;
+const coffeeCompany_model = require('./coffeeCompany_model')
+
+
+// app.get('/foo', function (req, res){
+//     res.send('Hello, World!')
+// });
+
+app.use(express.json())
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  next();
+});
+
+app.get('/coffee-companies/latest', (req, res) => {
+    coffeeCompany_model.getLatestCoffeeCompany()
+        .then(response => {
+            res.status(200).send(response);
+        })
+        .catch(error => {
+            res.status(500).send(error);
+        })
+})
+
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-});
-
-app.get('/foo', function (req, res){
-    res.send('Hello, World!')
-});
+    console.log(`App running on port ${port}.`)
+})
